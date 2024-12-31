9f54fa582aa38a22d73dbc4712d4e9f85b7adbd7
VanTu04 <141947683+VanTu04@users.noreply.github.com>
2024-12-31 14:28:37 +0700

    finish

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, KeyboardAvoidingView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Đăng nhập</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Nhập số điện thoại</Text>
+        <Text style={styles.subLabel}>Dùng số điện thoại để đăng nhập hoặc đăng ký tài khoản</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nhập số điện thoại"
+          keyboardType="phone-pad"
+        />
+      </View>
+      <Button style={styles.input_hover} title="Tiếp tục" onPress={() => {}} />
+    </KeyboardAvoidingView>
   );
 }
 
@@ -16,5 +27,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
+  header: {
+    marginBottom: 10,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    margin: 10,
+  },
+  inputContainer: {
+    width: "100%",
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  subLabel: {
+    fontSize: 14,
+    color: 'gray',
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+  },
+  input_hover: {
+    backgroundColor: 'green',
+    borderRadius: 10,
+  }
 });
---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Nguyễn Văn Tú - 22810310083
+# hình ảnh
+[bt2](./bt_2/assets/bt2.jpg)
